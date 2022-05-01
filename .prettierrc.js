249aa85232152800cabe320847696fbac8ebce3c
sl798970799 <798970799@qq.com>
2022-05-01 12:28:49 +0800

    feat: 初始化项目

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 800,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+};
---
+++ src/pages/canvas/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './index.less';
+
+function Canvas() {
+  return <div className={styles.canvas}>Canvas</div>;
+}
+
+export default Canvas;
